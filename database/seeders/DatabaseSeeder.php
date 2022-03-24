02e8058337db76c0b92ba6191bb08b98586485df
github naoki4287 <github@waka.naoki86@icloud.com>
2022-03-24 13:23:18 +0900

    ブログテーブルとダミーデータの用意をしました。

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Blog;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Blog::factory(30)->create();
     }
 }
